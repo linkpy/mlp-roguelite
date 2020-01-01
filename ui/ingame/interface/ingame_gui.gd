e18f4d96ec8ca71a46e0f7cbb64a133b94b5d6b2
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-01 19:34:08 +0100

    Added more comments.

--- ui/ingame/interface/ingame_gui.gd
+++ ui/ingame/interface/ingame_gui.gd
@@ -1,12 +1,16 @@
 extends Control
 
 
+### \description Path to the player node
 export var player_path: NodePath
 
+### \description Player instance
 onready var player = get_node(player_path)
+### \description Health bar instance
 onready var healthbar = $healthbar
 
 
 
+##################################################### _ready
 func _ready():
 	healthbar.set_value(50, 100)
