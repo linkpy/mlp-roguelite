0013de517990e4a890d5ed55c83f70f42d05460f
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-03 20:08:15 +0100

    Implemented the slime's attack_active state.

--- enemies/slime/state_attack_active.gd
+++ enemies/slime/state_attack_active.gd
@@ -1,2 +1,31 @@
 extends FSM_State
 
+
+
+func enter(data) -> void:
+	var direction = sign(
+		  object.player.position.x
+		- object.position.x
+	)
+	
+	object.animation.stop()
+	object.sprite.animation = "attack_active"
+	object.sprite.frame = 0
+	
+	object.velocity.y -= object.jump_impulse
+	object.velocity.x = direction * object.movement_speed
+
+
+
+func _physics_process(dt: float) -> void:
+	if object.velocity.y <= -1.5:
+		object.sprite.frame = 0
+	elif abs(object.velocity.y) < 1.5:
+		object.sprite.frame = 1
+	elif object.velocity.y >= 1.5:
+		object.sprite.frame = 2
+	
+	object.move(dt)
+	
+	if object.is_on_floor():
+		controller.state = "attack_recovery"
