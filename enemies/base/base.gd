c2747942c95b0635adc284236528d2da764f437a
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-03 19:39:31 +0100

    Added a player detection radius field for the base enemy class. Corrected the slime scene to use that new feature.

--- enemies/base/base.gd
+++ enemies/base/base.gd
@@ -1,6 +1,8 @@
 extends "res://scripts/physics/base_kinematic.gd"
 
 
+### \description Player detectin radius.
+export var player_detection_radius: float = 10
 
 ### \description Player detection area
 onready var player_detection = $player_detection
@@ -9,6 +11,12 @@ onready var player_detection = $player_detection
 
 ### \description Emitted when the player is in sight.
 signal player_sighted(p)
+
+
+
+##################################################### _ready
+func _ready() -> void:
+	$player_detection/shape.shape.radius = player_detection_radius
 
 
 
