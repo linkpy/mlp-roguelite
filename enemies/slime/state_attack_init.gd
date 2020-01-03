79f07ee02cc606ce621c6dc5018f9cfb593cd610
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-03 19:53:02 +0100

    Renamed the slime's attack state to attack_init.

---
+++ enemies/slime/state_attack_init.gd
@@ -0,0 +1,18 @@
+extends FSM_State
+
+
+
+###################################################### enter
+func enter(data) -> void:
+	object.animation = "attack_init"
+	
+	if object.player.position.x < object.position.x:
+		object.sprite.flip_h = true
+	else:
+		object.sprite.flip_h = false
+
+
+
+########################################### _physics_process
+func _physics_process(dt: float) -> void:
+	object.move(dt)
