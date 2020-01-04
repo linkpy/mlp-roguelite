80fd851459b47ea8f01946c8ff217529c96eb0a6
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-04 14:26:18 +0100

    Implemented damage system in the base entity.

--- scripts/entity/base_entity.gd
+++ scripts/entity/base_entity.gd
@@ -13,7 +13,51 @@ var health
 
 
 
+### \description Emitted when the entity is damaged
+signal damaged(origin, amnt)
+### \description Emitted when the entity is healed
+signal healed(origin, amnt)
+### \description Emitted when the entity is killed
+signal killed(origin)
+
+
+
 ##################################################### _ready
 func _ready():
 	stats.initialize()
 	health = stats.get_max_health()
+
+
+
+############################################################
+### \description Returns true if the entity is dead.
+###
+func is_dead() -> bool:
+	return health == 0
+
+
+
+############################################################
+### \description Applies damages to the entity.
+###
+### \param origin : entity applying the damages.
+### \param amnt : amount of applied damages.
+###
+func apply_damages(origin, amnt: float) -> void:
+	emit_signal("damaged", origin, amnt)
+	
+	health = max(0, health - amnt)
+	
+	if health == 0:
+		emit_signal("killed", origin)
+
+############################################################
+### \description Applies healing to the entity.
+###
+### \param origin : entity applying the heal.
+### \param amnt : amount of applied healing.
+###
+func apply_healing(origin, amnt: float) -> void:
+	emit_signal("healed", origin, amnt)
+	
+	health = min(stats.get_maximum_health(), health + amnt)
