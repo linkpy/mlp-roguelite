fb33fd1ffdae59c83f80c11fc3b873947c118055
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-04 13:41:04 +0100

    Renamed the physics folder to entity. Added the base entity class.

---
+++ scripts/entity/base_entity.gd
@@ -0,0 +1,10 @@
+### \description Base class for all damagable entities.
+###
+extends "res://scripts/entity/base_kinematic.gd"
+
+
+### \description Stats resource.
+export(Resource) var stats
+
+### \description Health of the entity.
+onready var health = stats.get_max_health()
