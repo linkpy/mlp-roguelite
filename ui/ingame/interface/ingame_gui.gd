42cbe7b2d3075e0fd81b2f467b272266fa194410
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-04 15:46:51 +0100

    Implemented automatic update of the player's heath bar.

--- ui/ingame/interface/ingame_gui.gd
+++ ui/ingame/interface/ingame_gui.gd
@@ -18,4 +18,18 @@ onready var healthbar = $healthbar
 
 ##################################################### _ready
 func _ready():
-	healthbar.set_value(50, 100)
+	player.connect("damaged", self, "_update_health_bar")
+	player.connect("healed", self, "_update_health_bar")
+	
+	_update_health_bar(null, null)
+
+
+
+############################################################
+### \description Update the health bar.
+###
+func _update_health_bar(_origin, _amnt):
+	healthbar.set_value(
+		player.health,
+		player.stats.get_max_health()
+	)
