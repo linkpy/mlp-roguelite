f18184e7eea5455637742e08de9ee4752c2ec18b
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-06 20:50:00 +0100

    Implemented attack chaining for the player attack1 state.

--- player/state_attack1.gd
+++ player/state_attack1.gd
@@ -16,11 +16,58 @@ const Attack = preload("res://player/attacks/player_attack1.tscn")
 
 
 
+### \description True if the attack should be chained with
+### the next one.
+var continue_attack: bool = false
+
+
+
 ###################################################### enter
 func enter(_data) -> void:
+	_configure_state()
+	_configure_player()
+	_spawn_attack()
+
+
+
+################################################### _process
+func _process(_dt: float) -> void:
+	if object.sprite.frame >= 3:
+		if Input.is_action_pressed("attack"):
+			continue_attack = true
+
+
+
+############################################################
+### \description Called when the player's animation player
+###              finished an animation.
+###
+func _on_animation_finished(anim: String):
+	if anim == "attack1":
+		if continue_attack:
+			controller.state = "attack2"
+		else:
+			controller.state = "normal"
+
+
+
+############################################################
+### \description Configure the state.
+###
+func _configure_state() -> void:
+	continue_attack = false
+
+############################################################
+### \description Configure the player
+###
+func _configure_player() -> void:
 	# start the attack animation of the player
 	object.animation.play("attack1")
-	
+
+############################################################
+### \description Spawn the attack instance.
+###
+func _spawn_attack() -> void:
 	# create the attack instance
 	var a = Attack.instance()
 	a.player = object
