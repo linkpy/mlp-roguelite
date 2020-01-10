e479e97ec959d88f5256328202f4eca1b5905388
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-01-10 09:37:35 +0100

    Added variations of the already made rooms.

--- scripts/level/renderer/builtins/render_random_one.gd
+++ scripts/level/renderer/builtins/render_random_one.gd
@@ -6,12 +6,25 @@ extends LevelRenderNode_BasicRenderer
 
 
 
+### \description If true, the node may render nothing
+export var may_render_nothing: bool = false
+
+
+
 ##################################################### render
 func render(ri: RoomInstance):
 	if get_child_count() == 0:
 		return
 	
 	var children = get_children()
-	var selected = children[randi() % children.size()]
+	var idx = (
+		  randi()
+		% (children.size() + (1 if may_render_nothing else 0))
+	)
+	
+	if idx >= children.size():
+		return
+	
+	var selected = children[idx]
 	
 	_render_child(ri, selected, false)
